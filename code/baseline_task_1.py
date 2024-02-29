db345121402ab1c5931b415d70ac4a9f715df6ba
allyson-y <54675313+allyson-y@users.noreply.github.com>
2024-02-29 10:48:31 -0800

    added some setup for bert training

--- code/baseline_task_1.py
+++ code/baseline_task_1.py
@@ -15,12 +15,26 @@ training set.
 
 import pandas as pd
 import numpy as np
+import re
 import sys
 import os
+
+# Taken from https://github.com/rajaswa/bert-humor-semeval-2020/blob/master/task1.py
+def get_sentence_pair(sent_orig, edit_word):
+    sent_o = re.sub("[</>]", "", sent_orig)
+    sent_e = (sent_orig.split("<"))[0] + edit_word + (sent_orig.split(">"))[1]
+
+    return sent_e, sent_o
 
 def baseline_task_1(train_loc, test_loc):
     train = pd.read_csv(train_loc)    
     test = pd.read_csv(test_loc)
+
+    sentences = train.original
+    edits = train.edit
+    for i in range(len(train)):
+        print(get_sentence_pair(sentences[i], edits[i]))
+    # can use these two sentences + meanGrade? to train on BERT?
 
     pred = np.mean(train.meanGrade)    
     test['pred'] = pred
